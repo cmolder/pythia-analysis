63e19f139b0da69f2aafb7db24d467b5843216ee
cmolder <28611108+cmolder@users.noreply.github.com>
2023-08-21 14:34:41 -0500

    Plotting support with Naboo

---
+++ plot_utils/collate.py
@@ -0,0 +1,81 @@
+import os
+from typing import Dict
+
+import pandas as pd
+
+import utils.file
+import utils.table
+
+class StudyCollator():
+    """"Helper class that gathers and organizes studies.
+    """
+    def __init__(self, sim_dir: str, study_name: str,
+                 baseline_study: str      = "stu_test",
+                 baseline_experiment: str = "_baseline",
+                 num_threads: int         =  16,
+                 weights_path: str        = "weights.toml"):
+        self.study_path = os.path.join(sim_dir, study_name)
+        self.baseline_study_path = os.path.join(sim_dir, baseline_study)
+
+        self.study = utils.file.ChampsimStudy(self.study_path)
+        self.baseline_study = utils.file.ChampsimStudy(self.baseline_study_path)
+        self.baseline_experiment = baseline_experiment
+
+        # Process the study
+        self.study.read_all(num_threads=num_threads,
+                            baseline_study=self.baseline_study,
+                            baseline_exp=self.baseline_experiment)
+
+        # Create a tabler and precompute benchmark/suite statistics.
+        self.tabler = utils.table.ChampsimTabler(self.study, 
+                                                 weights_path=weights_path)
+        self.tabler.generate_benchmark_statistics(num_threads=num_threads)
+        self.tabler.generate_suite_statistics(num_threads=num_threads)
+
+    def __getitem__(self, item):
+        assert isinstance(item, str), "StudyCollator must be indexed by experiment name"
+        return self.get_experiment(item)
+    
+    def __str__(self):
+        return f"StudyCollator({self.study_path})"
+    
+    def __repr__(self):
+        return str(self)
+
+    def get_experiment(self, experiment: str):
+        return ExperimentCollator(self, experiment)
+
+
+class ExperimentCollator():
+    """Helper class that gathers and organizes experiment statistics.
+    """
+    def __init__(self, study: StudyCollator, experiment_name: str):
+        self.study = study
+        self.experiment_name = experiment_name
+
+    def __str__(self):
+        return f"ExperimentCollator({self.study.study_path}, {self.experiment_name})"
+
+    def __repr__(self):
+        return str(self)
+
+    def get_suite_statistic(self, statistic_name: str):
+        table = self.study.tabler.table_suite_statistic(statistic_name)
+        return table.loc[self.experiment_name, :]
+
+    def get_benchmark_statistic(self, suite_name: str, statistic_name: str):
+        table = self.study.tabler.table_benchmark_statistic(statistic_name, suite_name)
+        return table[self.experiment_name]
+    
+
+def get_suite_statistic(experiments: Dict[str, ExperimentCollator], statistic_name: str):
+    rows = {}
+    for exp_name, exp in experiments.items():
+        rows[exp_name] = exp.get_suite_statistic(statistic_name)
+    return pd.DataFrame(rows)
+
+def get_benchmark_statistic(experiments: Dict[str, ExperimentCollator], suite_name: str, statistic_name: str):
+    rows = {}
+    for exp_name, exp in experiments.items():
+        rows[exp_name] = exp.get_benchmark_statistic(suite_name, statistic_name)
+    return pd.DataFrame(rows)
--- plot_utils/plot.py
+++ plot_utils/plot.py
@@ -1,19 +1,19 @@
 from collections import defaultdict
-from itertools import product
-import math
 from typing import Dict, List, Optional, Tuple
 
-import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 
-from utils import utils, tables
+from plot_utils import collate
 
 
 """
 Plotting helpers
 """
 def metric_tick_distances(metric: str) -> Optional[int]:
+    """Return the tick distance for a metric, if not provided by user.
+    """
     metric = metric.lower()
     if 'ipc_improvement' in metric:
         return 10
@@ -23,44 +23,74 @@ def metric_tick_distances(metric: str) -> Optional[int]:
         return 10
     else:
         return None
+    
+def get_mean_string(statistic_name: str):
+    """Infer the string for a mean statistic based on the
+    statistic name."""
+    if "gmean" in statistic_name:
+        return "gmean"
+    elif "hmean" in statistic_name:
+        return "hmean"
+    else:
+        return "amean"
+    
 
-"""
-Plotting functions
-"""
-def plot_metric(table: pd.DataFrame,
-                metric: Optional[str] = "",
-                suite: Optional[str] = "",
-                phase: Optional[str] = "",
-                secondary_table: Optional[pd.DataFrame] = None,
-                # Plotting kwargs
-                figsize: Tuple[int, int] = None,
-                dpi: int = None,
-                legend: bool = True,
-                add_suptitle: bool = True, suptitle: str = "",
-                add_xlabel: bool = True, xlabel: str = "Benchmark",
-                add_ylabel: bool = True, ylabel: str = "",
-                colors: defaultdict = defaultdict(lambda: None),
-                hatches: defaultdict = defaultdict(lambda: None),
-                annotations: defaultdict = defaultdict(str),
-                legend_kwargs: dict = {},
-                label_kwargs: dict = {},
-                ylim: Optional[Tuple[float, float]] = None,
-                ytick_distance: Optional[int] = None):
-    """Plot a specific metric for different prefetchers within a suite.
+def get_benchmark_table(experiments: Dict[str, collate.ExperimentCollator],
+                        suite_name: str,
+                        statistic_name: str,
+                        add_mean: bool = True) -> pd.DataFrame:
+    """Get the table for a benchmark, with some extra processing
+    for plotting.
+    """
+    table = collate.get_benchmark_statistic(experiments, suite_name, statistic_name).T
+    if add_mean:
+        mean = collate.get_suite_statistic(experiments, statistic_name).T
+        table[get_mean_string(statistic_name)] = mean[suite_name]
+    return table
+
+def get_suite_table(experiments: Dict[str, collate.ExperimentCollator],
+                    statistic_name: str) -> pd.DataFrame:
+    """Get the table for a suite, with some extra processing
+    for plotting.
+    """
+    table = collate.get_suite_statistic(experiments, statistic_name).T
+    return table
+
+def plot_table(table: pd.DataFrame,
+               secondary_table: Optional[pd.DataFrame] = None,
+               # Plotting kwargs
+               figsize: Tuple[int, int] = None,
+               dpi: int = None,
+               legend: bool = True,
+               add_suptitle: bool = True, suptitle: str = "",
+               add_xlabel: bool = True, xlabel: str = "Benchmark",
+               add_ylabel: bool = True, ylabel: str = "",
+               colors: defaultdict = defaultdict(lambda: None),
+               secondary_colors: List[str] = ["lightgray"],
+               hatches: defaultdict = defaultdict(lambda: None),
+               annotations: defaultdict = defaultdict(str),
+               legend_kwargs: dict = {},
+               label_kwargs: dict = {},
+               ylim: Optional[Tuple[float, float]] = None,
+               ytick_distance: Optional[int] = None):
+    """Plot a statistic for different experiments.
 
     Parameters:
-        data_df: A dict of prefetchers and their statistics dataframes.
-        metric: The metric to plot.
-        suite: The name of the suite.
-        dpi: The matplotlib DPI.
+        table: A table of metrics.
+        secondary_table: A table of secondary metrics. (TODO: Support mutliple secondary tables)
+
+    Plotting kwargs:
         figsize: The matplotlib figsize.
-        
-    Returns: None
+        dpi: The matplotlib DPI.
+        TODO: Describe the rest
     """
+    
     Xs, ys, ys_secondary = {}, {}, {}
     min_y, max_y = 0.0, 0.0
     num_entries, num_traces = len(table), len(table.columns)
     gap, margin = 2, 2
+
+    # Plot
     for i, (index, row) in enumerate(table.iterrows()):
         Xs[index] = (np.arange(num_traces) * (num_entries + gap)) + i
         ys[index] = row.values
@@ -76,7 +106,9 @@ def plot_metric(table: pd.DataFrame,
     fig, ax = plt.subplots(figsize=figsize, dpi=dpi)
     for k, x in Xs.items():
         if k in ys_secondary:
-            ax.bar(x, ys_secondary[k], color='lightgray', width=1.0, edgecolor='gray', linewidth=0.3, zorder=0.99)
+            ax.bar(x, ys_secondary[k], color=secondary_colors[0], 
+                   width=1.0, edgecolor='gray', linewidth=0.3, 
+                   zorder=0.99)
         ax.bar(x, ys[k], label=k, color=colors[k], hatch=hatches[k], 
                edgecolor='black', linewidth=0.3, width=1.0)
 
@@ -95,15 +127,13 @@ def plot_metric(table: pd.DataFrame,
         ax.set_xlabel(xlabel)
 
     # Y-axis
-    ytick_dist = metric_tick_distances(metric) if ytick_distance is None else ytick_distance
     ylim_lower, ylim_upper = (min_y, max_y) if ylim is None else (ylim[0], ylim[1])
-    if ytick_dist is not None:
+    if ytick_distance is not None:
         round_to_multiple = lambda num, mul : mul * round(num / mul)
-        ax.set_yticks(np.arange(round_to_multiple(ylim_lower, ytick_dist), round_to_multiple(ylim_upper, ytick_dist) + 1, ytick_dist))
+        ax.set_yticks(np.arange(round_to_multiple(ylim_lower, ytick_distance), round_to_multiple(ylim_upper, ytick_distance) + 1, ytick_distance))
         ax.tick_params(axis='y', labelsize=8)
     ax.set_ylim(None if ylim is None else ylim_lower, None if ylim is None else ylim_upper)
     if add_ylabel:
-        ylabel = metric.replace('_', ' ') if ylabel == '' else ylabel
         ax.set_ylabel(ylabel, fontsize=8)
 
     # Grid
@@ -115,17 +145,54 @@ def plot_metric(table: pd.DataFrame,
         ax.legend(**legend_kwargs)
 
     # Title
-    if add_suptitle:
-        title = f'{suptitle} {metric.replace("_", " ")}'
-        if suite != '' and phase != '':
-            title += f' ({suite} {phase})'
-        elif suite != '':
-            title += f' ({suite})'
-        fig.suptitle(title)
+    # TODO: Fix title
+    # if add_suptitle:
+    #     title = f'{suptitle} {metric.replace("_", " ")}'
+    #     if suite != '' and phase != '':
+    #         title += f' ({suite} {phase})'
+    #     elif suite != '':
+    #         title += f' ({suite})'
+    #     fig.suptitle(title)
     
     fig.tight_layout()
 
+"""
+Plotting functions
+"""
+def plot_benchmark_statistic(experiments: Dict[str, collate.ExperimentCollator],
+                             suite_name: str,
+                             statistic_name: str,
+                             secondary_statistic_name: Optional[str] = None,
+                             add_mean: bool = True,
+                             **kwargs):
+    """TODO: Docstring
+    """
+    # Gather table(s)
+    table = get_benchmark_table(experiments, suite_name, statistic_name, 
+                                add_mean=add_mean)
+    if secondary_statistic_name is not None:
+        secondary_table = get_benchmark_table(experiments, suite_name, secondary_statistic_name, 
+                                              add_mean=add_mean)
+    else:
+        secondary_table = None
 
+    plot_table(table, secondary_table, **kwargs)
+
+def plot_suite_statistic(experiments: Dict[str, collate.ExperimentCollator],
+                         statistic_name: str,
+                         secondary_statistic_name: Optional[str] = None,
+                         **kwargs):
+    """TODO: Docstring
+    """
+    table = get_suite_table(experiments, statistic_name)
+    if secondary_statistic_name is not None:
+        secondary_table = get_suite_table(experiments, secondary_statistic_name)
+    else:
+        secondary_table = None
+
+    plot_table(table, secondary_table, **kwargs)
+
+'''
 def plot_overpredictions(cov_table: pd.DataFrame,
                          cov_late_table: pd.DataFrame,
                          overpred_table: pd.DataFrame,
@@ -364,3 +431,4 @@ def plot_everything_overpredictions(data_df: Dict[str, pd.DataFrame],
         overpred_table.columns = [utils.clean_trace_name(tr) for tr in overpred_table.columns]
 
         plot_overpredictions(cov_table, cov_late_table, overpred_table, **kwargs)
+'''
